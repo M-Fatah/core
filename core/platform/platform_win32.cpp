a4fafbdfa1f7b7af065599c22a616b0bdfbee661
M-Fatah <MuhammadA.Fatah@gmail.com>
2022-06-28 02:06:32 +0200

    Final cleanup.

--- core/platform/platform_win32.cpp
+++ core/platform/platform_win32.cpp
@@ -394,10 +394,12 @@ platform_window_poll(Platform_Window *self)
 }
 
 void
-platform_window_get_native_handles(Platform_Window *self, void **native_handle, void **)
+platform_window_get_native_handles(Platform_Window *self, void **native_handle, void **native_display)
 {
 	if (native_handle)
 		*native_handle = self->handle;
+	if (native_display)
+		*native_display = nullptr;
 }
 
 void
