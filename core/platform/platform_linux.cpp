25a9c8da94ae184e2c4f54bec95f3c9460dc1ad1
Muhammad Abdulfattah <MuhammadA.Fatah@gmail.com>
2022-08-28 19:33:38 +0200

    Add keyboard keys support to Linux. (#6)

--- core/platform/platform_linux.cpp
+++ core/platform/platform_linux.cpp
@@ -13,8 +13,9 @@
 #include <execinfo.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
-#include <xcb/xcb.h>
 #include <X11/Xlib-xcb.h>
+#include <X11/keysym.h>
+#include <X11/XKBlib.h>
 
 typedef void * (*platform_api_proc)(void *api, bool reload);
 
@@ -31,6 +32,118 @@ _string_concat(const char *a, const char *b, char *result)
 	{
 		*result++ = *b++;
 	}
+}
+
+inline static PLATFORM_KEY
+_platform_key_from_xcb_button(xcb_button_t button)
+{
+	switch (button)
+	{
+		case XCB_BUTTON_INDEX_1: return PLATFORM_KEY_MOUSE_LEFT;
+		case XCB_BUTTON_INDEX_2: return PLATFORM_KEY_MOUSE_MIDDLE;
+		case XCB_BUTTON_INDEX_3: return PLATFORM_KEY_MOUSE_RIGHT;
+		case XCB_BUTTON_INDEX_4: return PLATFORM_KEY_MOUSE_WHEEL_UP;
+		case XCB_BUTTON_INDEX_5: return PLATFORM_KEY_MOUSE_WHEEL_DOWN;
+	}
+	return PLATFORM_KEY_COUNT;
+}
+
+inline static PLATFORM_KEY
+_platform_key_from_key_sym(KeySym key)
+{
+	switch (key)
+	{
+		case XK_a:            return PLATFORM_KEY_A;
+		case XK_b:            return PLATFORM_KEY_B;
+		case XK_c:            return PLATFORM_KEY_C;
+		case XK_d:            return PLATFORM_KEY_D;
+		case XK_e:            return PLATFORM_KEY_E;
+		case XK_f:            return PLATFORM_KEY_F;
+		case XK_g:            return PLATFORM_KEY_G;
+		case XK_h:            return PLATFORM_KEY_H;
+		case XK_i:            return PLATFORM_KEY_I;
+		case XK_j:            return PLATFORM_KEY_J;
+		case XK_k:            return PLATFORM_KEY_K;
+		case XK_l:            return PLATFORM_KEY_L;
+		case XK_m:            return PLATFORM_KEY_M;
+		case XK_n:            return PLATFORM_KEY_N;
+		case XK_o:            return PLATFORM_KEY_O;
+		case XK_p:            return PLATFORM_KEY_P;
+		case XK_q:            return PLATFORM_KEY_Q;
+		case XK_r:            return PLATFORM_KEY_R;
+		case XK_s:            return PLATFORM_KEY_S;
+		case XK_t:            return PLATFORM_KEY_T;
+		case XK_u:            return PLATFORM_KEY_U;
+		case XK_v:            return PLATFORM_KEY_V;
+		case XK_w:            return PLATFORM_KEY_W;
+		case XK_x:            return PLATFORM_KEY_X;
+		case XK_y:            return PLATFORM_KEY_Y;
+		case XK_z:            return PLATFORM_KEY_Z;
+		case XK_0:            return PLATFORM_KEY_NUM_0;
+		case XK_1:            return PLATFORM_KEY_NUM_1;
+		case XK_2:            return PLATFORM_KEY_NUM_2;
+		case XK_3:            return PLATFORM_KEY_NUM_3;
+		case XK_4:            return PLATFORM_KEY_NUM_4;
+		case XK_5:            return PLATFORM_KEY_NUM_5;
+		case XK_6:            return PLATFORM_KEY_NUM_6;
+		case XK_7:            return PLATFORM_KEY_NUM_7;
+		case XK_8:            return PLATFORM_KEY_NUM_8;
+		case XK_9:            return PLATFORM_KEY_NUM_9;
+		case XK_KP_0:         return PLATFORM_KEY_NUMPAD_0;
+		case XK_KP_1:         return PLATFORM_KEY_NUMPAD_1;
+		case XK_KP_2:         return PLATFORM_KEY_NUMPAD_2;
+		case XK_KP_3:         return PLATFORM_KEY_NUMPAD_3;
+		case XK_KP_4:         return PLATFORM_KEY_NUMPAD_4;
+		case XK_KP_5:         return PLATFORM_KEY_NUMPAD_5;
+		case XK_KP_6:         return PLATFORM_KEY_NUMPAD_6;
+		case XK_KP_7:         return PLATFORM_KEY_NUMPAD_7;
+		case XK_KP_8:         return PLATFORM_KEY_NUMPAD_8;
+		case XK_KP_9:         return PLATFORM_KEY_NUMPAD_9;
+		case XK_F1:           return PLATFORM_KEY_F1;
+		case XK_F2:           return PLATFORM_KEY_F2;
+		case XK_F3:           return PLATFORM_KEY_F3;
+		case XK_F4:           return PLATFORM_KEY_F4;
+		case XK_F5:           return PLATFORM_KEY_F5;
+		case XK_F6:           return PLATFORM_KEY_F6;
+		case XK_F7:           return PLATFORM_KEY_F7;
+		case XK_F8:           return PLATFORM_KEY_F8;
+		case XK_F9:           return PLATFORM_KEY_F9;
+		case XK_F10:          return PLATFORM_KEY_F10;
+		case XK_F11:          return PLATFORM_KEY_F11;
+		case XK_F12:          return PLATFORM_KEY_F12;
+		case XK_Up:           return PLATFORM_KEY_ARROW_UP;
+		case XK_Down:         return PLATFORM_KEY_ARROW_DOWN;
+		case XK_Left:         return PLATFORM_KEY_ARROW_LEFT;
+		case XK_Right:        return PLATFORM_KEY_ARROW_RIGHT;
+		case XK_Shift_L:      return PLATFORM_KEY_SHIFT_LEFT;
+		case XK_Shift_R:      return PLATFORM_KEY_SHIFT_RIGHT;
+		case XK_Control_L:    return PLATFORM_KEY_CONTROL_LEFT;
+		case XK_Control_R:    return PLATFORM_KEY_CONTROL_RIGHT;
+		case XK_Alt_L:        return PLATFORM_KEY_ALT_LEFT;
+		case XK_Alt_R:        return PLATFORM_KEY_ALT_RIGHT;
+		case XK_BackSpace:    return PLATFORM_KEY_BACKSPACE;
+		case XK_Tab:          return PLATFORM_KEY_TAB;
+		case XK_Return:       return PLATFORM_KEY_ENTER;
+		case XK_Escape:       return PLATFORM_KEY_ESCAPE;
+		case XK_Delete:       return PLATFORM_KEY_DELETE;
+		case XK_Insert:       return PLATFORM_KEY_INSERT;
+		case XK_Home:         return PLATFORM_KEY_HOME;
+		case XK_End:          return PLATFORM_KEY_END;
+		case XK_Page_Up:      return PLATFORM_KEY_PAGE_UP;
+		case XK_Page_Down:    return PLATFORM_KEY_PAGE_DOWN;
+		case XK_slash:        return PLATFORM_KEY_SLASH;
+		case XK_backslash:    return PLATFORM_KEY_BACKSLASH;
+		case XK_bracketleft:  return PLATFORM_KEY_BRACKET_LEFT;
+		case XK_bracketright: return PLATFORM_KEY_BRACKET_RIGHT;
+		case XK_grave:        return PLATFORM_KEY_BACKQUOTE;
+		case XK_period:       return PLATFORM_KEY_PERIOD;
+		case XK_minus:        return PLATFORM_KEY_MINUS;
+		case XK_equal:        return PLATFORM_KEY_EQUAL;
+		case XK_comma:        return PLATFORM_KEY_COMMA;
+		case XK_semicolon:    return PLATFORM_KEY_SEMICOLON;
+		case XK_space:        return PLATFORM_KEY_SPACE;
+	}
+	return PLATFORM_KEY_COUNT;
 }
 
 Platform_Api
@@ -172,7 +285,7 @@ platform_window_init(u32 width, u32 height, const char *title)
 
 	const struct xcb_setup_t *setup = ::xcb_get_setup(connection);
 	xcb_screen_iterator_t iterator  = ::xcb_setup_roots_iterator(setup);
-	xcb_screen_t *screen             = iterator.data;
+	xcb_screen_t *screen            = iterator.data;
 
 	xcb_window_t window = ::xcb_generate_id(connection);
 
@@ -224,6 +337,9 @@ platform_window_init(u32 width, u32 height, const char *title)
 
 	::xcb_change_property(connection, XCB_PROP_MODE_REPLACE, window, wm_protocols_reply->atom, 4, 32, 1, &wm_delete_reply->atom);
 
+	// Set auto repeat keys to off.
+	::XAutoRepeatOff(display);
+
 	// Map the window to the screen.
 	::xcb_map_window(connection, window);
 
@@ -255,16 +371,13 @@ platform_window_deinit(Platform_Window *self)
 {
 	Platform_Window_Context *ctx = (Platform_Window_Context *)self->handle;
 
+	// Set auto repeat keys back to on.
+	::XAutoRepeatOn(ctx->display);
 	::xcb_destroy_window(ctx->connection, ctx->window);
-	::xcb_disconnect(ctx->connection);
 
 	memory::deallocate(ctx);
 }
 
-/*
-	TODO:
-	- [ ] Implement key presses/releases.
-*/
 bool
 platform_window_poll(Platform_Window *self)
 {
@@ -272,136 +385,120 @@ platform_window_poll(Platform_Window *self)
 
 	for (i32 i = 0; i < PLATFORM_KEY_COUNT; ++i)
 	{
-		self->input.keys[i].pressed  = false;
-		self->input.keys[i].released = false;
+		self->input.keys[i].pressed       = false;
+		self->input.keys[i].released      = false;
+		self->input.keys[i].press_count   = 0;
+		self->input.keys[i].release_count = 0;
 	}
 	self->input.mouse_wheel = 0.0f;
 
-	XEvent event = {};
-	while (XPending(ctx->display))
+	while (xcb_generic_event_t *xcb_event = ::xcb_poll_for_event(ctx->connection))
 	{
-		XNextEvent(ctx->display, &event);
-		if (XFilterEvent(&event, None))
-			return true;
-
-		switch (event.type)
+		switch (xcb_event->response_type & ~0x80)
 		{
-			case ClientMessage:
+			case XCB_CLIENT_MESSAGE:
 			{
-				if ((xcb_atom_t)event.xclient.data.l[0] == ctx->wm_delete_window_atom)
+				xcb_client_message_event_t *xcb_client_message = (xcb_client_message_event_t *)xcb_event;
+				if (xcb_client_message->data.data32[0] == ctx->wm_delete_window_atom)
 					return false;
 				break;
 			}
-			case KeyPress:
+			case XCB_KEY_PRESS:
 			{
-				break;
-			}
-			case KeyRelease:
-			{
-				break;
-			}
-			case ButtonPress:
-			{
-				switch (event.xbutton.button)
+				xcb_key_press_event_t *xcb_key_press_event = (xcb_key_press_event_t *)xcb_event;
+				KeySym key_sym = ::XkbKeycodeToKeysym(ctx->display, (KeyCode)xcb_key_press_event->detail, 0, 0);
+
+				PLATFORM_KEY key = _platform_key_from_key_sym(key_sym);
+				if (key != PLATFORM_KEY_COUNT)
 				{
-					case Button1:
-					{
-						self->input.keys[PLATFORM_KEY_MOUSE_LEFT].pressed = true;
-						self->input.keys[PLATFORM_KEY_MOUSE_LEFT].down    = true;
-						self->input.keys[PLATFORM_KEY_MOUSE_LEFT].press_count++;
-						break;
-					}
-					case Button2:
-					{
-						self->input.keys[PLATFORM_KEY_MOUSE_MIDDLE].pressed = true;
-						self->input.keys[PLATFORM_KEY_MOUSE_MIDDLE].down    = true;
-						self->input.keys[PLATFORM_KEY_MOUSE_MIDDLE].press_count++;
-						break;
-					}
-					case Button3:
-					{
-						self->input.keys[PLATFORM_KEY_MOUSE_RIGHT].pressed = true;
-						self->input.keys[PLATFORM_KEY_MOUSE_RIGHT].down    = true;
-						self->input.keys[PLATFORM_KEY_MOUSE_RIGHT].press_count++;
-						break;
-					}
-					case Button4:
-					{
+					self->input.keys[key].pressed  = true;
+					self->input.keys[key].down     = true;
+					self->input.keys[key].press_count++;
+				}
+				break;
+			}
+			case XCB_KEY_RELEASE:
+			{
+				xcb_key_release_event_t *xcb_key_release_event = (xcb_key_release_event_t *)xcb_event;
+				KeySym key_sym = ::XkbKeycodeToKeysym(ctx->display, (KeyCode)xcb_key_release_event->detail, 0, 0);
+
+				PLATFORM_KEY key = _platform_key_from_key_sym(key_sym);
+				if (key != PLATFORM_KEY_COUNT)
+				{
+					self->input.keys[key].released = true;
+					self->input.keys[key].down     = false;
+					self->input.keys[key].release_count++;
+				}
+				break;
+			}
+			case XCB_BUTTON_PRESS:
+			{
+				xcb_button_press_event_t *xcb_mouse_press_event = (xcb_button_press_event_t *)xcb_event;
+
+				PLATFORM_KEY key = _platform_key_from_xcb_button(xcb_mouse_press_event->detail);
+				if (key != PLATFORM_KEY_COUNT)
+				{
+					self->input.keys[key].pressed = true;
+					self->input.keys[key].down    = true;
+					self->input.keys[key].press_count++;
+					if (key == PLATFORM_KEY_MOUSE_WHEEL_UP)
 						self->input.mouse_wheel += 1.0f;
-						break;
-					}
-					case Button5:
-					{
-						self->input.mouse_wheel += -1.0f;
-						break;
-					}
+					else if (key == PLATFORM_KEY_MOUSE_WHEEL_DOWN)
+						self->input.mouse_wheel -= 1.0f;
 				}
 				break;
 			}
-			case ButtonRelease:
+			case XCB_BUTTON_RELEASE:
 			{
-				switch (event.xbutton.button)
+				xcb_button_release_event_t *xcb_mouse_release_event = (xcb_button_release_event_t *)xcb_event;
+
+				PLATFORM_KEY key = _platform_key_from_xcb_button(xcb_mouse_release_event->detail);
+				if (key != PLATFORM_KEY_COUNT)
 				{
-					case Button1:
-					{
-						self->input.keys[PLATFORM_KEY_MOUSE_LEFT].released = true;
-						self->input.keys[PLATFORM_KEY_MOUSE_LEFT].down     = false;
-						self->input.keys[PLATFORM_KEY_MOUSE_LEFT].release_count++;
-						break;
-					}
-					case Button2:
-					{
-						self->input.keys[PLATFORM_KEY_MOUSE_MIDDLE].released = true;
-						self->input.keys[PLATFORM_KEY_MOUSE_MIDDLE].down     = false;
-						self->input.keys[PLATFORM_KEY_MOUSE_MIDDLE].release_count++;
-						break;
-					}
-					case Button3:
-					{
-						self->input.keys[PLATFORM_KEY_MOUSE_RIGHT].released = true;
-						self->input.keys[PLATFORM_KEY_MOUSE_RIGHT].down     = false;
-						self->input.keys[PLATFORM_KEY_MOUSE_RIGHT].release_count++;
-						break;
-					}
+					self->input.keys[key].released = true;
+					self->input.keys[key].down     = false;
+					self->input.keys[key].release_count++;
 				}
 				break;
 			}
-			case ConfigureNotify:
+			case XCB_CONFIGURE_NOTIFY:
 			{
-				if (self->width != (u32)event.xconfigure.width || self->height != (u32)event.xconfigure.height)
+				xcb_configure_notify_event_t *xcb_configure_notify_event = (xcb_configure_notify_event_t *)xcb_event;
+				if (self->width != (u32)xcb_configure_notify_event->width || self->height != (u32)xcb_configure_notify_event->height)
 				{
-					self->width = event.xconfigure.width;
-					self->height = event.xconfigure.height;
+					self->width  = xcb_configure_notify_event->width;
+					self->height = xcb_configure_notify_event->height;
 				}
 				break;
 			}
 			default:
 				break;
 		}
+
+		::free(xcb_event);
 	}
 
-	// NOTE: Mouse movement.
 	{
-		Window root_window;
-		Window child_window;
-		i32 root_window_mouse_x;
-		i32 root_window_mouse_y;
-		i32 window_mouse_x;
-		i32 window_mouse_y;
-		u32 window_mask;
-		XQueryPointer(ctx->display, ctx->window, &root_window, &child_window, &root_window_mouse_x, &root_window_mouse_y, &window_mouse_x, &window_mouse_y, &window_mask);
+		// NOTE: Mouse movement.
+		xcb_query_pointer_cookie_t xcb_query_pointer_cookie = ::xcb_query_pointer_unchecked(ctx->connection, ctx->window);
+		xcb_query_pointer_reply_t *xcb_query_pointer_reply  = ::xcb_query_pointer_reply(ctx->connection, xcb_query_pointer_cookie, nullptr);
 
+		i32 window_mouse_x = xcb_query_pointer_reply->win_x;
+		i32 window_mouse_y = xcb_query_pointer_reply->win_y;
 		if (window_mouse_x >= 0 && (u32)window_mouse_x < self->width && window_mouse_y >= 0 && (u32)window_mouse_y < self->height)
 		{
 			if (window_mouse_x != self->input.mouse_x || window_mouse_y != self->input.mouse_y)
 			{
+				// NOTE: We want mouse coords to start bottom-left.
 				u32 mouse_point_y_inverted = (self->height - 1) - window_mouse_y;
 				self->input.mouse_dx = window_mouse_x - self->input.mouse_x;
 				self->input.mouse_dy = self->input.mouse_y - mouse_point_y_inverted;
 				self->input.mouse_x  = window_mouse_x;
-				self->input.mouse_y  = mouse_point_y_inverted; // NOTE(M-Fatah): We want mouse coords to start bottom-left.
+				self->input.mouse_y  = mouse_point_y_inverted;
 			}
 		}
+
+		::free(xcb_query_pointer_reply);
 	}
 
 	return true;
@@ -425,13 +522,14 @@ platform_window_set_title(Platform_Window *self, const char *title)
 	::xcb_change_property(ctx->connection, XCB_PROP_MODE_REPLACE, ctx->window, XCB_ATOM_WM_NAME, XCB_ATOM_STRING, 8, ::strlen(title), title);
 }
 
+// TODO: Do we need this?
 void
 platform_window_close(Platform_Window *self)
 {
 	Platform_Window_Context *ctx = (Platform_Window_Context *)self->handle;
 
 	XEvent event;
-	::memset(&event, 0, sizeof (event));
+	::memset(&event, 0, sizeof(event));
 	event.xclient.type         = ClientMessage;
 	event.xclient.window       = ctx->window;
 	event.xclient.message_type = ::XInternAtom(ctx->display, "WM_PROTOCOLS", true);
