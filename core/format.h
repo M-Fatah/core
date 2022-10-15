64bf7779c7818e8db3d216c2534b3766aebcfcd7
M-Fatah <MuhammadA.Fatah@gmail.com>
2022-10-15 02:33:17 +0200

    Cleanup.

--- core/format.h
+++ core/format.h
@@ -8,19 +8,10 @@
 
 /*
 	TODO:
-	- [x] Overload 'format' function like serializer instead?
-	- [x] Remove libfmt dependency.
-	- [x] Collapse the two 'formatter_format' functions.
-	- [x] Collapse the two 'formatter_parse' functions.
-	- [x] Add unittests.
-	- [x] Add support for arrays.
-	- [x] Template types names differ between Windows/Linux.
-	- [x] Check for matching count of replacement_characters and argument count.
-	- [x] Move implementation to .cpp file.
 	- [ ] Remove the 32KB buffer size restriction.
-	- [ ] Simplify and optimize.
 	- [ ] Do not rely on ::snprintf and implement our own formatting.
 	- [ ] Pointer formatting differ between Windows/Linux.
+	- [ ] Try and workaround moving implementation to cpp file.
 */
 
 #define FORMAT(T) \
@@ -53,46 +44,9 @@ struct Formatter
 	clear();
 };
 
-inline static void
-formatter_clear(Formatter &self)
-{
-	self.clear();
-}
-
-#undef FORMAT
-
-template <typename T>
-inline static void
-format(Formatter &, T)
-{
-	static_assert(sizeof(T) == 0, "There is no `void format(Formatter &, T)` function overload defined for this type.");
-}
-
-#define FORMAT(T)               \
-inline static void              \
-format(Formatter &self, T data) \
-{                               \
-    self.format(data);          \
-}                               \
-
-FORMAT(i8)
-FORMAT(i16)
-FORMAT(i32)
-FORMAT(i64)
-FORMAT(u8)
-FORMAT(u16)
-FORMAT(u32)
-FORMAT(u64)
-FORMAT(f32)
-FORMAT(f64)
-FORMAT(bool)
-FORMAT(char)
-
-#undef FORMAT
-
 template <typename ...TArgs>
 inline static void
-format(Formatter &self, const char *fmt, const TArgs &...args)
+formatter_format(Formatter &self, const char *fmt, const TArgs &...args)
 {
 	u64 fmt_count = ::strlen(fmt);
 	if (fmt_count == 0)
@@ -156,17 +110,25 @@ format(Formatter &self, const char *fmt, const TArgs &...args)
 
 		if (fmt[i] == '{' && fmt[i + 1] == '}')
 		{
-			// TODO: For now we will eat the replacement characters and do nothing.
 			if (self.depth == 0)
 			{
+				//
+				// NOTE:
+				// The replacement character count is larger than the number of passed arguments,
+				//    at this point we just eat them.
+				//
 				i++;
 				self.replacement_character_count++;
 			}
 			else
 			{
+				//
+				// NOTE:
+				// The user passed "{}" replacement character as an argument, we just append it,
+				//    for e.x. formatter_format(formatter, "{}", "{}"); => "{}".
+				//
 				self.buffer[self.index++] = '{';
 				self.buffer[self.index++] = '}';
-
 			}
 			continue;
 		}
@@ -190,7 +152,50 @@ format(Formatter &self, const char *fmt, const TArgs &...args)
 	self.buffer[self.index] = '\0';
 }
 
-// TODO: Can we move this to cpp file?
+inline static void
+formatter_clear(Formatter &self)
+{
+	self.clear();
+}
+
+#undef FORMAT
+
+template <typename T>
+inline static void
+format(Formatter &, const T &)
+{
+	static_assert(sizeof(T) == 0, "There is no `void format(Formatter &, const T &)` function overload defined for this type.");
+}
+
+#define FORMAT(T)               \
+inline static void              \
+format(Formatter &self, T data) \
+{                               \
+    self.format(data);          \
+}                               \
+
+FORMAT(i8)
+FORMAT(i16)
+FORMAT(i32)
+FORMAT(i64)
+FORMAT(u8)
+FORMAT(u16)
+FORMAT(u32)
+FORMAT(u64)
+FORMAT(f32)
+FORMAT(f64)
+FORMAT(bool)
+FORMAT(char)
+
+#undef FORMAT
+
+template <typename ...TArgs>
+inline static void
+format(Formatter &self, const char *fmt, const TArgs &...args)
+{
+	formatter_format(self, fmt, args...);
+}
+
 template <typename T>
 concept Pointer_Type = std::is_pointer_v<T>;
 
@@ -204,13 +209,12 @@ format(Formatter &self, const T data)
 		self.format((const void *)data);
 }
 
-// TODO: Can we move this to cpp file?
 template <typename T>
 concept Array_Type = !std::is_same_v<T, char>;
 
 template <Array_Type T, u64 N>
 inline static void
-format(Formatter &self, const T(&data)[N])
+format(Formatter &self, const T (&data)[N])
 {
 	u64 count = N;
 	format(self, "[{}] {{ ", count);
