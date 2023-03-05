c341da37609af46c2dd6e242a2dd537a19050315
M-Fatah <MuhammadA.Fatah@gmail.com>
2023-03-05 16:38:24 +0200

    Cleanup.

--- core/reflect.h
+++ core/reflect.h
@@ -17,21 +17,25 @@
 		- [x] Arrays.
 		- [x] Structs and Classes.
 			- [x] Empty structs and classes.
+			- [ ] Simplify OVERLOARD(TYPE_OF_FIELD, __VA_ARGS__) and use FOR_EACH() macro?
+			- [ ] Handle private variables inside classes by overloading member functions?
 		- [x] Enums.
 			- [ ] Add enum range?
 			- [ ] What if enum were used as flags?
 			- [ ] Add ability for the user to define enum range?
 			- [ ] What if the user used weird assignment values (for example => ENUM_ZERO = 0, ENUM_THREE = 3, ENUM_TWO = 2)?
-			- [ ] Preserve the order of enum values.
+				- [ ] Preserve the order of enum values.
 			- [ ] Enums with the same value?
 			- [ ] Enum class?
+			- [ ] Simplify type_of(Enum).
+	- [ ] Simplify, optimize and cleanup name_of<T>().
 	- [ ] Name as reflect/reflector/reflection?
-	- [ ] Create global constexpr values for enum range and name length.
-	- [ ] Try to constexpr everything.
-	- [ ] Try to get rid of std includes.
 	- [ ] Use string_view for names and avoid allocations at all?
 	- [ ] Cleanup.
 */
+
+inline static constexpr const u64 REFLECT_MAX_NAME_LENGTH      = 64;
+inline static constexpr const u64 REFLECT_MAX_ENUM_VALUE_COUNT = 32;
 
 enum TYPE_KIND
 {
@@ -47,10 +51,10 @@ enum TYPE_KIND
 	TYPE_KIND_F64,
 	TYPE_KIND_BOOL,
 	TYPE_KIND_CHAR,
-	TYPE_KIND_STRUCT,
-	TYPE_KIND_ARRAY,
 	TYPE_KIND_POINTER,
-	TYPE_KIND_ENUM
+	TYPE_KIND_ARRAY,
+	TYPE_KIND_ENUM,
+	TYPE_KIND_STRUCT
 };
 
 struct Type_Field
@@ -70,9 +74,8 @@ struct Type
 	{
 		struct
 		{
-			const Type_Field *fields;
-			u64 field_count;
-		} as_struct;
+			const Type *pointee;
+		} as_pointer;
 		struct
 		{
 			const Type *element;
@@ -80,14 +83,15 @@ struct Type
 		} as_array;
 		struct
 		{
-			const Type *pointee;
-		} as_pointer;
-		struct
-		{
 			const i32 *indices;
-			const char (*names)[1024];
+			const char (*names)[REFLECT_MAX_NAME_LENGTH];
 			u64 element_count;
 		} as_enum;
+		struct
+		{
+			const Type_Field *fields;
+			u64 field_count;
+		} as_struct;
 	};
 };
 
@@ -96,44 +100,6 @@ struct Value
 	const void *data;
 	const Type *type;
 };
-
-template <typename T>
-inline static constexpr TYPE_KIND
-kind_of()
-{
-	if constexpr (std::is_same_v<T, i8>)
-		return TYPE_KIND_I8;
-	else if constexpr (std::is_same_v<T, i16>)
-		return TYPE_KIND_I16;
-	else if constexpr (std::is_same_v<T, i32>)
-		return TYPE_KIND_I32;
-	else if constexpr (std::is_same_v<T, i64>)
-		return TYPE_KIND_I64;
-	else if constexpr (std::is_same_v<T, u8>)
-		return TYPE_KIND_U8;
-	else if constexpr (std::is_same_v<T, u16>)
-		return TYPE_KIND_U16;
-	else if constexpr (std::is_same_v<T, u32>)
-		return TYPE_KIND_U32;
-	else if constexpr (std::is_same_v<T, u64>)
-		return TYPE_KIND_U64;
-	else if constexpr (std::is_same_v<T, f32>)
-		return TYPE_KIND_F32;
-	else if constexpr (std::is_same_v<T, f64>)
-		return TYPE_KIND_F64;
-	else if constexpr (std::is_same_v<T, bool>)
-		return TYPE_KIND_BOOL;
-	else if constexpr (std::is_same_v<T, char>)
-		return TYPE_KIND_CHAR;
-	else if constexpr (std::is_array_v<T>)
-		return TYPE_KIND_ARRAY;
-	else if constexpr (std::is_pointer_v<T>)
-		return TYPE_KIND_POINTER;
-	else if constexpr (std::is_enum_v<T>)
-		return TYPE_KIND_ENUM;
-	else if constexpr (std::is_compound_v<T>)
-		return TYPE_KIND_STRUCT;
-}
 
 template <typename T>
 inline static constexpr const char *
@@ -163,7 +129,12 @@ name_of()
 			#endif
 		};
 
-		constexpr auto sanitise_name = [](char (&name)[1024], u64 &i, u64 &count, const std::string_view &type_name) {
+		constexpr auto get_unified_name = [](char (&name)[REFLECT_MAX_NAME_LENGTH], u64 &i, u64 &count, const std::string_view &type_name) {
+			constexpr auto string_append = [](char (&name)[REFLECT_MAX_NAME_LENGTH], const char *n, u64 &count) {
+				while(*n != '\0')
+					name[count++] = *n++;
+			};
+
 			auto c = type_name.data()[i];
 			if (c == '<' || c == ',')
 			{
@@ -180,99 +151,73 @@ name_of()
 				std::string_view nn = {type_name.data() + i, diff};
 				if (nn == "signed char")
 				{
-					name[count++] = 'i';
-					name[count++] = '8';
-					i += 11;
+					string_append(name, "i8", count);
+					i += nn.length();
 				}
 				else if (nn == "short")
 				{
-					name[count++] = 'i';
-					name[count++] = '1';
-					name[count++] = '6';
-					i += 5;
+					string_append(name, "i16", count);
+					i += nn.length();
 				}
 				else if (nn == "short int")
 				{
-					name[count++] = 'i';
-					name[count++] = '1';
-					name[count++] = '6';
-					i += 9;
+					string_append(name, "i16", count);
+					i += nn.length();
 				}
 				else if (nn == "int")
 				{
-					name[count++] = 'i';
-					name[count++] = '3';
-					name[count++] = '2';
-					i += 3;
+					string_append(name, "i32", count);
+					i += nn.length();
 				}
 				else if (nn == "__int64")
 				{
-					name[count++] = 'i';
-					name[count++] = '6';
-					name[count++] = '4';
-					i += 7;
+					string_append(name, "i64", count);
+					i += nn.length();
 				}
 				else if (nn == "long int")
 				{
-					name[count++] = 'i';
-					name[count++] = '6';
-					name[count++] = '4';
-					i += 8;
+					string_append(name, "i64", count);
+					i += nn.length();
 				}
 				else if (nn == "unsigned char")
 				{
-					name[count++] = 'u';
-					name[count++] = '8';
-					i += 13;
+					string_append(name, "u8", count);
+					i += nn.length();
 				}
 				else if (nn == "unsigned short")
 				{
-					name[count++] = 'u';
-					name[count++] = '1';
-					name[count++] = '6';
-					i += 14;
+					string_append(name, "u16", count);
+					i += nn.length();
 				}
 				else if (nn == "short unsigned int")
 				{
-					name[count++] = 'u';
-					name[count++] = '1';
-					name[count++] = '6';
-					i += 18;
+					string_append(name, "u16", count);
+					i += nn.length();
 				}
 				else if (nn == "unsigned int")
 				{
-					name[count++] = 'u';
-					name[count++] = '3';
-					name[count++] = '2';
-					i += 12;
+					string_append(name, "u32", count);
+					i += nn.length();
 				}
 				else if (nn == "unsigned __int64")
 				{
-					name[count++] = 'u';
-					name[count++] = '6';
-					name[count++] = '4';
-					i += 16;
+					string_append(name, "u64", count);
+					i += nn.length();
 				}
 				else if (nn == "long unsigned int")
 				{
-					name[count++] = 'u';
-					name[count++] = '6';
-					name[count++] = '4';
-					i += 17;
+					string_append(name, "u64", count);
+					i += nn.length();
 				}
 				else if (nn == "float")
 				{
-					name[count++] = 'f';
-					name[count++] = '3';
-					name[count++] = '2';
-					i += 5;
+					string_append(name, "f32", count);
+					i += nn.length();
 				}
 				else if (nn == "double")
 				{
-					name[count++] = 'f';
-					name[count++] = '6';
-					name[count++] = '4';
-					i += 6;
+					string_append(name, "f64", count);
+					i += nn.length();
 				}
 				else if (nn.starts_with("enum "))
 				{
@@ -289,8 +234,8 @@ name_of()
 			}
 		};
 
-		constexpr auto get_type_name = [sanitise_name](const std::string_view &type_name) -> const char * {
-			static char name[1024] = {};
+		constexpr auto get_type_name = [get_unified_name](const std::string_view &type_name) -> const char * {
+			static char name[REFLECT_MAX_NAME_LENGTH] = {};
 			for (u64 i = 0, count = 0; i < type_name.length(); ++i)
 			{
 				std::string_view n = {type_name.data() + i, type_name.length() - i};
@@ -301,12 +246,11 @@ name_of()
 				else if (n.starts_with("struct "))
 					i += 7;
 
-				// TODO: Cleanup.
 				if (type_name.data()[i] != ' ' && i < type_name.length() && count < sizeof(name))
 				{
-					sanitise_name(name, i, count, type_name);
+					get_unified_name(name, i, count, type_name);
 					if (type_name.data()[i] == ',')
-						sanitise_name(name, i, count, type_name);
+						get_unified_name(name, i, count, type_name);
 					name[count++] = type_name.data()[i];
 				}
 			}
@@ -321,6 +265,44 @@ name_of()
 
 		return get_type_name({type_function_name.data() + type_name_prefix_length, type_name_length});
 	}
+}
+
+template <typename T>
+inline static constexpr TYPE_KIND
+kind_of()
+{
+	if constexpr (std::is_same_v<T, i8>)
+		return TYPE_KIND_I8;
+	else if constexpr (std::is_same_v<T, i16>)
+		return TYPE_KIND_I16;
+	else if constexpr (std::is_same_v<T, i32>)
+		return TYPE_KIND_I32;
+	else if constexpr (std::is_same_v<T, i64>)
+		return TYPE_KIND_I64;
+	else if constexpr (std::is_same_v<T, u8>)
+		return TYPE_KIND_U8;
+	else if constexpr (std::is_same_v<T, u16>)
+		return TYPE_KIND_U16;
+	else if constexpr (std::is_same_v<T, u32>)
+		return TYPE_KIND_U32;
+	else if constexpr (std::is_same_v<T, u64>)
+		return TYPE_KIND_U64;
+	else if constexpr (std::is_same_v<T, f32>)
+		return TYPE_KIND_F32;
+	else if constexpr (std::is_same_v<T, f64>)
+		return TYPE_KIND_F64;
+	else if constexpr (std::is_same_v<T, bool>)
+		return TYPE_KIND_BOOL;
+	else if constexpr (std::is_same_v<T, char>)
+		return TYPE_KIND_CHAR;
+	else if constexpr (std::is_pointer_v<T>)
+		return TYPE_KIND_POINTER;
+	else if constexpr (std::is_array_v<T>)
+		return TYPE_KIND_ARRAY;
+	else if constexpr (std::is_enum_v<T>)
+		return TYPE_KIND_ENUM;
+	else if constexpr (std::is_compound_v<T>)
+		return TYPE_KIND_STRUCT;
 }
 
 template <typename T>
@@ -385,14 +367,46 @@ TYPE_OF(f64)
 TYPE_OF(bool)
 TYPE_OF(char)
 
-// TODO: Simplify and properly name variables.
+template <typename T>
+requires (std::is_pointer_v<T>)
+inline static constexpr const Type *
+type_of(const T)
+{
+	static const Type *pointee = nullptr;
+	if constexpr (not std::is_same_v<std::remove_pointer_t<T>, void>)
+		pointee = type_of(std::remove_pointer_t<T>{});
+	static const Type self = {
+		.name = name_of<T>(),
+		.kind = kind_of<T>(),
+		.size = sizeof(T),
+		.align = alignof(T),
+		.as_pointer = pointee
+	};
+	return &self;
+}
+
+template <typename T, u64 N>
+inline static constexpr const Type *
+type_of(const T(&)[N])
+{
+	static const Type self = {
+		.name = name_of<T[N]>(),
+		.kind = kind_of<T[N]>(),
+		.size = sizeof(T[N]),
+		.align = alignof(T[N]),
+		.as_array = {
+			type_of(T{}),
+			N
+		}
+	};
+	return &self;
+}
+
 template <typename T>
 requires (std::is_enum_v<T>)
 inline static constexpr const Type *
 type_of(const T)
 {
-	constexpr const u64 ENUM_VALUE_COUNT_MAX = 30;
-
 	struct Enum_Value
 	{
 		i32 index;
@@ -401,7 +415,7 @@ type_of(const T)
 
 	struct Enum
 	{
-		std::array<Enum_Value, ENUM_VALUE_COUNT_MAX> values;
+		std::array<Enum_Value, REFLECT_MAX_ENUM_VALUE_COUNT> values;
 		u64 count;
 	};
 
@@ -429,11 +443,11 @@ type_of(const T)
 			{get_enum_value.template operator()<(T)I>()...},
 			((get_enum_value.template operator()<(T)I>().name != "") + ...)
 		};
-	}(std::make_integer_sequence<i32, ENUM_VALUE_COUNT_MAX>());
+	}(std::make_integer_sequence<i32, REFLECT_MAX_ENUM_VALUE_COUNT>());
 
 	static i32 indices[data.count] = {};
-	static char names[data.count][1024] = {};
-	for (u64 i = 0, c = 0; i < ENUM_VALUE_COUNT_MAX; ++i)
+	static char names[data.count][REFLECT_MAX_NAME_LENGTH] = {};
+	for (u64 i = 0, c = 0; i < REFLECT_MAX_ENUM_VALUE_COUNT; ++i)
 	{
 		if (const auto &value = data.values[i]; value.name != "")
 		{
@@ -453,41 +467,6 @@ type_of(const T)
 			.names = names,
 			.element_count = data.count
 		}
-	};
-	return &self;
-}
-
-template <typename T, u64 N>
-inline static constexpr const Type *
-type_of(const T(&)[N])
-{
-	static const Type self = {
-		.name = name_of<T[N]>(),
-		.kind = kind_of<T[N]>(),
-		.size = sizeof(T[N]),
-		.align = alignof(T[N]),
-		.as_array = {
-			type_of(T{}),
-			N
-		}
-	};
-	return &self;
-}
-
-template <typename T>
-requires (std::is_pointer_v<T>)
-inline static constexpr const Type *
-type_of(const T)
-{
-	static const Type *pointee = nullptr;
-	if constexpr (not std::is_same_v<std::remove_pointer_t<T>, void>)
-		pointee = type_of(std::remove_pointer_t<T>{});
-	static const Type self = {
-		.name = name_of<T>(),
-		.kind = kind_of<T>(),
-		.size = sizeof(T),
-		.align = alignof(T),
-		.as_pointer = pointee
 	};
 	return &self;
 }
