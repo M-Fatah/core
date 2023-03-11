9477b2050a65a9d64db586c39879c65ccaf2698b
M-Fatah <MuhammadA.Fatah@gmail.com>
2023-03-11 05:13:28 +0200

    Fix unittests.

--- core/reflect.h
+++ core/reflect.h
@@ -43,7 +43,7 @@
 	- [ ] Cleanup.
 */
 
-inline static constexpr const u64 REFLECT_MAX_NAME_LENGTH      =  64;
+inline static constexpr const u64 REFLECT_MAX_NAME_LENGTH      = 128;
 inline static constexpr const i32 REFLECT_MIN_ENUM_VALUE       = -32;
 inline static constexpr const i32 REFLECT_MAX_ENUM_VALUE       =  32;
 inline static constexpr const i32 REFLECT_MAX_ENUM_VALUE_COUNT = REFLECT_MAX_ENUM_VALUE - REFLECT_MIN_ENUM_VALUE;
