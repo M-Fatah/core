03ade9c4f2cc27d052d7e3984a4a308c26679b4e
M-Fatah <MuhammadA.Fatah@gmail.com>
2024-10-18 20:55:39 +0200

    Add unitest for JSON serializer.

--- unittest/src/unittest_serializer.cpp
+++ unittest/src/unittest_serializer.cpp
@@ -1,5 +1,6 @@
 // TODO:
 #include "../../test/src/bin_serializer.h"
+#include "../../test/src/jsn_serializer.h"
 
 #include <doctest/doctest.h>
 
@@ -149,4 +150,60 @@ TEST_CASE("[CORE]: Binary_Serializer")
 	}
 
 	// CHECK(platform_file_delete("serialize_test.core"));
+}
+
+TEST_CASE("[CORE]: JSON_Serializer")
+{
+	Jsn_Serializer serializer = jsn_serializer_init();
+	DEFER(jsn_serializer_deinit(serializer));
+
+	Game original_game = game_init();
+	DEFER(game_deinit(original_game));
+
+	{
+		// Set some arbitrary values.
+		{
+			original_game.a = 31;
+			original_game.b = 37;
+			original_game.c = 1.5f;
+			original_game.d = 'A';
+			array_push(original_game.e, 0.5f);
+			array_push(original_game.e, 1.5f);
+			array_push(original_game.e, 2.5f);
+			string_append(original_game.f, "Hello1");
+			hash_table_insert(original_game.g, string_literal("1"), 1.0f);
+			hash_table_insert(original_game.g, string_literal("2"), 2.0f);
+			hash_table_insert(original_game.g, string_literal("3"), 3.0f);
+		}
+
+		serialize(serializer, {"original_game", original_game});
+
+		{
+			const char *expected_json_string = R"""({"original_game":{"a":31,"b":37,"c":1.500000,"d":65,"e":[0.500000,1.500000,2.500000],"f":"Hello1","g":[{"key":"1","value":1.000000},{"key":"2","value":2.000000},{"key":"3","value":3.000000}]}})""";
+			CHECK(serializer.buffer == expected_json_string);
+		}
+
+		{
+			Game new_game = {};
+			DEFER(game_deinit(new_game));
+			deserialize(serializer, {"original_game", new_game});
+
+			CHECK(new_game.a == original_game.a);
+			CHECK(new_game.b == original_game.b);
+			CHECK(new_game.c == original_game.c);
+			CHECK(new_game.d == original_game.d);
+
+			for (u64 i = 0; i < new_game.e.count; ++i)
+				CHECK(new_game.e[i] == original_game.e[i]);
+
+			CHECK(new_game.f == original_game.f);
+
+			for (const auto &[new_key, new_value] : new_game.g)
+			{
+				const auto &[original_key, original_value] = *hash_table_find(original_game.g, new_key);
+				CHECK(new_key == original_key);
+				CHECK(new_value == original_value);
+			}
+		}
+	}
 }
