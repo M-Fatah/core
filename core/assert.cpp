c44f869da88924a81aa309883e4385e625ef6396
M-Fatah <MuhammadA.Fatah@gmail.com>
2024-12-04 22:24:09 +0100

    Add an overloard to handle assert without message.

--- core/assert.cpp
+++ core/assert.cpp
@@ -12,11 +12,14 @@ assert(bool expression, const char *message, std::source_location source_locatio
 
 		LOG_FATAL("Assertion failure: '{}', message: '{}', file: '{}', line: '{}'.", expression, message, source_location.file_name(), source_location.line());
 
+		// TODO: Print stack.
+
 		#if _MSC_VER // TODO: Add compiler definition.
 			#define DEBUG_BREAK() __debugbreak()
 		#else
 			#define DEBUG_BREAK() __builtin_trap()
 		#endif
-
+	#else
+		unused(expression, message, source_location);
 	#endif
 }
--- core/assert.h
+++ core/assert.h
@@ -4,5 +4,13 @@
 
 #include <source_location>
 
+#undef assert
+
 CORE_API void
 assert(bool expression, const char *message, std::source_location source_location = std::source_location::current());
+
+inline static void
+assert(bool expression, std::source_location source_location = std::source_location::current())
+{
+	assert(expression, "", source_location);
+}
