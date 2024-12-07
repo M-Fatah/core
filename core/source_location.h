129e4d3aa7608675f344a2d58a4391816ac2c924
M-Fatah <MuhammadA.Fatah@gmail.com>
2024-12-07 03:44:56 +0100

    Remove column from source location.

--- core/source_location.h
+++ core/source_location.h
@@ -1,23 +1,20 @@
 #pragma once
 
 #include "core/defines.h"
-#include <source_location>
 
 struct Source_Location
 {
 	const char *file_name;
 	const char *function_name;
 	u32 line_number;
-	u32 column_number;
 };
 
 inline static consteval Source_Location
-source_location_get(const char *file_name = __builtin_FILE(), const char *function_name = __builtin_FUNCTION(), u32 line_number = __builtin_LINE(), u32 column_number = __builtin_COLUMN())
+source_location_get(const char *file_name = __builtin_FILE(), const char *function_name = __builtin_FUNCTION(), u32 line_number = __builtin_LINE())
 {
 	return Source_Location {
 		.file_name = file_name,
 		.function_name = function_name,
-		.line_number = line_number,
-		.column_number = column_number
+		.line_number = line_number
 	};
 }
